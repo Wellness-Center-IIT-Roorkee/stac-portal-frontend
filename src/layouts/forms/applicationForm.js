7902be8c5ca0435e4568220185fde2aa37fadd6c
Ashwani Kumar <ashwamikumar94665@gmail.com>
2021-12-29 15:41:26 +0530

    some bugs fixed

--- src/layouts/forms/applicationForm.js
+++ src/layouts/forms/applicationForm.js
@@ -7,8 +7,9 @@ import { Stack,Select, MenuItem} from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 
 const ApplicationForm = () => {
+    const userData = useSelector(state => state.users.userData);
     const [inputs, setInputs] = useState({
-        number:"",
+        number: userData?.phone_number,
         supervisorEmail:"",
         hodEmail:"",
     });
@@ -18,23 +19,22 @@ const ApplicationForm = () => {
         const value = event.target.value;
         setInputs(values => ({...values, [name]: value}))
     }
-
+    
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(inputs);
     }
 
-    const userData = useSelector(state => state.users.userData);
     
     return (
             <div>
             <form className='wl-st-form'>
                 <div>
-                    <FormDisabled title='Name' value={userData.name} ph='Enter your name' />
-                    <FormDisabled title='Enrollment Number' value={userData.student.enrollment_number} ph='Enter your enrollment number' />
-                    <FormDisabled title='Email Id' value={userData.email} ph='Enter your email' />
-                    <FormDisabled title='Department/Centre' value={userData.student.department} ph='Enter your department or centre' />
-                    <FormDisabled title='Branch' value={userData.student.branch} ph='Enter your branch' />
+                    <FormDisabled title='Name' value={userData?.name} ph='Enter your name' />
+                    <FormDisabled title='Enrollment Number' value={userData?.student.enrollment_number} ph='Enter your enrollment number' />
+                    <FormDisabled title='Email Id' value={userData?.email} ph='Enter your email' />
+                    <FormDisabled title='Department/Centre' value={userData?.student.department} ph='Enter your department or centre' />
+                    <FormDisabled title='Branch' value={userData?.student.branch} ph='Enter your branch' />
                     <div className='wl-st-form-box'>
                         <label className='info-name'>Semester applying for </label><br></br>
                         <br></br>
@@ -43,7 +43,7 @@ const ApplicationForm = () => {
                             <MenuItem value='Spring'>Spring</MenuItem>
                         </Select>
                     </div>
-                    <FormDisabled value={userData.phone_number} title='Mobile Number' type='number' ph='Enter your number' name='number'/>
+                    <FormBox value={inputs?.number} title='Mobile Number' type='number' ph='Enter your number' name='number'/>
                     <FormBox title="Supervisor's Email Id" type='email' ph='Enter email' name='supervisorEmail' value={inputs.supervisorEmail || ""} onChange={handleChange}/>
                     <FormBox title='Head of Dept. Email Id' type='email' ph='Enter email' name='hodEmail' value={inputs.hodEmail || ""} onChange={handleChange}/>
 
